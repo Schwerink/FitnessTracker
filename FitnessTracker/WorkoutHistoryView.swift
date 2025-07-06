1ef47a395c60e47e3c9f8d447532c95d600ee8c8
Schwerik <erik.weitzel04@web.de>
2025-07-06 13:27:47 +0200

    Histroy view

---
+++ FitnessTracker/WorkoutHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutHistoryView.swift
+//  FitnessTracker
+//
+//  Created by Erik on 05.07.25.
+//
+
+import SwiftUI
+
+struct WorkoutHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutHistoryView()
+}
